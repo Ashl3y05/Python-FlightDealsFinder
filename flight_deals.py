b63aa7f3da67e0b35b74f786c1d455dfa2613e16
Ashley <panelo.ashley@gmail.com>
2025-06-18 20:47:43 +0800

    Added destination_data.py

--- flight_deals.py
+++ flight_deals.py
@@ -8,4 +8,4 @@ class FlightDeals:
         self.amadeus_secret_key = os.environ["AMADEUS_SECRET"]
         self.amadeus_base_endpoint = "http:/test.api.amadeus.com/v2"
         self.amadeus_access_token = os.environ["AMADEUS_ACCESS_TOKEN"]
-
+        self.flight_offers_endpoint = "/shopping/flight_offers"
