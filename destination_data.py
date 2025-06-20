268cd09690795fe7d9ee78b175f74682595fdbc4
Ashley <panelo.ashley@gmail.com>
2025-06-20 20:38:38 +0800

    Can now update the excel with the cheapest flight staring next day

--- destination_data.py
+++ destination_data.py
@@ -5,7 +5,6 @@ import os
 class DestinationData:
     def __init__(self):
         self.sheety_endpoint = "https://api.sheety.co/76ca9e17df4224ea156829a794c915ee/flightDeals/sheet1"
-        self.sheety_endpoint2 = "https://api.sheety.co/76ca9e17df4224ea156829a794c915ee/flightDeals/sheet1/2"
 
 
     def get_excel_data(self) -> dict:
@@ -19,15 +18,20 @@ class DestinationData:
         all_destinations = [data["sheet1"][num]["city"] for num in range(0, len(data["sheet1"]))]
         return all_destinations
 
-    def put_excel_data(self, city: str, iata_code: str, lowest_price: int):
+    def put_excel_data(self, city: str, iata_code: str, lowest_price: float,row_id: int):
         """Updates the Excel file"""
         edit_excel = {
             "sheet1": {
                 "city": city,
                 "iataCode": iata_code,
-                "lowestPrice": str(lowest_price)
+                "lowestPrice": lowest_price
             }
 
         }
-        response = requests.put(url=self.sheety_endpoint2, json=edit_excel)
-        print("Updated!")
+        response = requests.put(url=f"{self.sheety_endpoint}/{row_id}", json=edit_excel)
+        print(f"Updated {city} - {iata_code} - {lowest_price}")
+
+    def get_last_price(self,row_id):
+        data = self.get_excel_data()
+        return data["sheet1"][row_id - 1]["lowestPrice"]
+
--- flight_deals.py
+++ flight_deals.py
@@ -1,14 +1,14 @@
 import requests
 import os
-from datetime import datetime
+
 
 class FlightDeals:
     def __init__(self):
         self.amadeus_api_key = os.environ["AMADEUS_API_KEY"]
         self.amadeus_secret_key = os.environ["AMADEUS_SECRET"]
-        self.amadeus_base_endpoint = "http:/test.api.amadeus.com/v2"
+        self.amadeus_base_endpoint = "https://test.api.amadeus.com/v2"
         self.amadeus_access_token = self.get_access_token()
-        self.flight_offers_endpoint = "/shopping/flight_offers"
+        self.flight_offers_endpoint = "/shopping/flight-offers"
         self.city_search_endpoint = "https://test.api.amadeus.com/v1/reference-data/locations/cities"
 
     def get_access_token(self):
@@ -28,12 +28,31 @@ class FlightDeals:
         return f"{result["token_type"]} {result["access_token"]}"
 
     def search_city(self, city: str, max_result: int):
+
         data = {
             "keyword": city,
             "max":str(max_result)
         }
+
         header = {
             "Authorization": self.amadeus_access_token
         }
         response = requests.get(url=self.city_search_endpoint,params=data,headers=header)
+        return response.json()
+
+    def get_cheapest_offer(self, origin_city_code, destination_city_code,departure_date):
+        header = {
+            "Authorization": self.amadeus_access_token
+        }
+
+        data = {
+            "originLocationCode":origin_city_code,
+            "destinationLocationCode":destination_city_code,
+            "departureDate":departure_date,
+            "adults": 1,
+            "currencyCode": "PHP",
+            "max": 5
+        }
+
+        response = requests.get(url=f"{self.amadeus_base_endpoint}{self.flight_offers_endpoint}", params=data,headers=header)
         return response.json()
