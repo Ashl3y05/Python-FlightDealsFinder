76c88ddeb1b910846d0fc6d4f09f122dd29e70c1
Ashley <panelo.ashley@gmail.com>
2025-06-27 23:44:26 +0800

    Added codes to send_email method

--- user_manager.py
+++ user_manager.py
@@ -4,6 +4,9 @@ import smtplib
 class UserManager:
     def __init__(self):
         self.sheety_endpoint = "https://api.sheety.co/76ca9e17df4224ea156829a794c915ee/flightDeals/users"
+        self.sender_email = "youremail@gmail.com"
+        self.receiver_email = "recipient@example.com"
+        self.password = "your_app_password"
 
     def get_user_data(self):
         response = requests.get(url=self.sheety_endpoint)
@@ -16,4 +19,8 @@ class UserManager:
 
 
     def send_email(self):
+        with smtplib.SMTP("smtp.gmail.com", 587) as server:
+            server.starttls()
+            server.login(self.sender_email, self.password)
+            server.send_message()
         pass
